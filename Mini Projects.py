2284ec42b057c3f0976fa55781c8c2cb6d7944df
Assalamalikum! Muhammad Anus Khan <muhammadanuskhan23@gmail.com>
2026-03-12 03:03:49 +0500

    Mini Projects

--- Mini Projects.py
+++ Mini Projects.py
@@ -1,18 +1,32 @@
 import random
+import string
 
-target=random.randint(1,100)
+# # -------------------------------
+# # guess the number
+# # -------------------------------
+# target=random.randint(1,100)
 
-while True:
-    UserNumber=input("Guess the number or Quit(Q): ")
-    if(UserNumber=="Q"):
-        break
-    UserNumber=input("Guess the number: ")
-    if(UserNumber==target):
-        print("Success : Correct Guess!!")
-        break
-    if(UserNumber<target):
-        print("Your Number is too small.")
-    else:
-        print("Your NNumber is too large.")
+# while True:
+#     UserNumber=input("Guess the number or Quit(Q): ")
+#     if(UserNumber=="Q"):
+#         break
+#     UserNumber=input("Guess the number: ")
+#     if(UserNumber==target):
+#         print("Success : Correct Guess!!")
+#         break
+#     if(UserNumber<target):
+#         print("Your Number is too small.")
+#     else:
+#         print("Your NNumber is too large.")
 
-print("-----------Game Over----------")
+# print("-----------Game Over----------")
+
+# ---------------------------------
+# Password Generator
+# ---------------------------------
+characters=string.ascii_letters + string.digits + string.punctuation
+password_lenght=12
+password=""
+for i in range(password_lenght):
+    password+=random.choice(characters)
+print(password)
